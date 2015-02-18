bc68a47ebfe2002e977b58d1c3085ec0b52e5d31
edzye101 <edzye101@gmail.com>
2015-02-17 21:58:38 -0800

    ported to Ghub

--- www/js/services.js
+++ www/js/services.js
@@ -3,9 +3,9 @@ angular.module('starter.services', [])
  * A simple example service that returns some data.
  */
 .factory('fireBaseData', function($firebase) {
-  var ref = new Firebase("https://luminous-fire-3429.firebaseio.com/"),
-      refExpenses = new Firebase("https://luminous-fire-3429.firebaseio.com/expenses"),
-      refRoomMates = new Firebase("https://luminous-fire-3429.firebaseio.com/room-mates");
+  var ref = new Firebase("https://instxpense.firebaseio.com/"),
+      refExpenses = new Firebase("https://instxpense.firebaseio.com/expenses"),
+      refRoomMates = new Firebase("https://instxpense.firebaseio.com/room-mates");
   return {
     ref: function () {
       return ref;
